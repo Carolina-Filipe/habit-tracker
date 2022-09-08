c95b833689899eb2bc7fdbde3b4ed59ab373f335
Carolina Filipe <anilorac.filipe@gmail.com>
2022-09-08 17:12:20 +0200

    form

--- src/components/newhabitform.tsx
+++ src/components/newhabitform.tsx
@@ -1,10 +1,40 @@
-const NewHabitForm = () => {
-  const submitHandler = console.log;
+import { useRef, useState } from "react";
+
+const NewHabitForm = (props: any) => {
+  const [habitName, setHabitName] = useState("");
+  //   const submitHandler = (event: any) => {
+  //     event.preventDefault();
+  //     const habitInput = event.current.value;
+
+  //     if (habitInput.trim().length > 0) {
+  //       props.onEnterTask(habitInput);
+  //     }
+
+  //     console.log;
+  //   };
+
+  //todo: give this the correct type
+  const handleHabitNameChange = (event: any) => {
+    console.log(event.target.value);
+    setHabitName(event.target.value);
+  };
+
+  //todo: give this the correct type
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    console.log("submit", habitName);
+  };
+
   return (
-    <form onSubmit={submitHandler}>
+    <form>
       New habit form
-      <input className="border-gray-500" type="text"></input>
-      <button>Add new habit</button>
+      <input
+        className="border-gray-500"
+        type="text"
+        value={habitName}
+        onChange={handleHabitNameChange}
+      ></input>
+      <button onClick={handleSubmit}>Add new habit</button>
     </form>
   );
 };
