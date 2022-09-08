093c760d0c0a41dc7647842a01d41e52532733b0
Carolina Filipe <anilorac.filipe@gmail.com>
2022-09-08 11:42:03 +0200

    list of elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 import "./App.css";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Habit Tracker</h1>;
+  return (
+    <div className="flex p-8 gap-8">
+      <h1 className="text-3xl font-bold underline">Habit Tracker</h1>
+      <div className="block">
+        <form>New habit form</form>
+        <button>Add new habit</button>
+      </div>
+      <div>
+        List of habits
+        <div>
+          Habit Object
+          <div>Habit title</div>
+          <div> Habit id</div>
+          <div> Habit checkbox</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
