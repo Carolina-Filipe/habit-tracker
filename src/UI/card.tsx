f00550ee5145da1eb93e3d3f91d78e94ed1dbae6
Carolina Filipe <anilorac.filipe@gmail.com>
2022-09-19 10:27:29 +0200

    progress bar start

--- src/UI/card.tsx
+++ src/UI/card.tsx
@@ -1,7 +1,17 @@
 const Card = (props: any) => {
+  const percentage = 0.1;
+
+  const cardWdith = 0;
+  // const progressWidth = cardWdith * percentage;
+  const progressWidth = "400px";
+
   return (
-    <div className="flex inline-flex w-full bg-light_green p-4 flex-col rounded-2xl">
-      {props.children}
+    <div className="flex inline-flex w-full bg-paper flex-col rounded-2xl -z-20">
+      <div
+        className="rounded-2xl p-4 h-full -z-10"
+        style={{ background: "red", width: progressWidth }}
+      ></div>
+      <div className="z-10">{props.children}</div>
     </div>
   );
 };
