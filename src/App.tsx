84c8817cf4487ea70383cbd63fed00274b573daf
Carolina Filipe <anilorac.filipe@gmail.com>
2022-09-19 10:39:13 +0200

    send habits to localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,16 @@ import NewHabitForm from "./components/newhabitform";
 import { Habit } from "./types/habit";
 
 function App() {
+  const storedHabits = JSON.parse(localStorage.getItem("Habits") || "{}");
+  // try to load the array from localstorage if not there then empty array under useState
   const [habits, setHabits] = useState<Habit[]>([]);
   const addHabitsHandler = (habit: Habit) => {
-    // console.log(habit);
     setHabits((prevHabits) => {
       return [habit, ...prevHabits];
     });
   };
+  console.log(habits);
+  localStorage.setItem("Habits", JSON.stringify(habits));
 
   return (
     <div className="flex flex-col w-full justify-center items-center p-8 gap-8">
@@ -21,7 +24,7 @@ function App() {
         <NewHabitForm className="gap-8" onAddHabit={addHabitsHandler} />
       </div>
       <div className="w-3/4">
-        <Habits habits={habits} />
+        <Habits habits={storedHabits} />
       </div>
       <Footer />
     </div>
