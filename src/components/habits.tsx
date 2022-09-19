64978e2d10e7e480de5a68b0fa537119ace45d9d
Carolina Filipe <anilorac.filipe@gmail.com>
2022-09-19 12:15:15 +0200

    handling undefined

--- src/components/habits.tsx
+++ src/components/habits.tsx
@@ -2,13 +2,13 @@ import HabitItem from "./habitItem";
 
 const Habits = (props: any) => {
   //to-do: figure out props type
-  if (props.habits.length === 0) {
+  if (props.habits?.length === 0) {
     return <h2>Found no results.</h2>;
   }
   // console.log(props.habits, "habits file");
   return (
     <div className="flex flex-col gap-8">
-      {props.habits.map((habit: any) => (
+      {props.habits?.map((habit: any) => (
         <HabitItem
           title={habit.name.charAt(0).toUpperCase() + habit.name.slice(1)}
           key={habit.index}
