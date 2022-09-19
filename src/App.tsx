3e8d35b64b3cad22a1646e2e5dc8e18287d93238
Carolina Filipe <anilorac.filipe@gmail.com>
2022-09-19 12:33:34 +0200

    loading habits array from localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Footer from "./components/footer";
 import Habits from "./components/habits";
@@ -6,16 +6,31 @@ import NewHabitForm from "./components/newhabitform";
 import { Habit } from "./types/habit";
 
 function App() {
-  const storedHabits = JSON.parse(localStorage.getItem("Habits") || "{}");
   // try to load the array from localstorage if not there then empty array under useState
-  const [habits, setHabits] = useState<Habit[]>([]);
+  const [habits, setHabits] = useState<Habit[]>();
+
+  useEffect(() => {
+    const storedHabits = JSON.parse(localStorage.getItem("Habits") || "[]");
+    console.log(storedHabits);
+
+    setHabits(storedHabits);
+  }, []);
+
+  useEffect(() => {
+    console.log(habits);
+
+    if (habits) {
+      localStorage.setItem("Habits", JSON.stringify(habits));
+    }
+  }, [habits]);
+
   const addHabitsHandler = (habit: Habit) => {
+    console.log("add", habit);
+
     setHabits((prevHabits) => {
-      return [habit, ...prevHabits];
+      return [habit, ...(prevHabits ?? [])];
     });
   };
-  console.log(habits);
-  localStorage.setItem("Habits", JSON.stringify(habits));
 
   return (
     <div className="flex flex-col w-full justify-center items-center p-8 gap-8">
@@ -24,7 +39,7 @@ function App() {
         <NewHabitForm className="gap-8" onAddHabit={addHabitsHandler} />
       </div>
       <div className="w-3/4">
-        <Habits habits={storedHabits} />
+        <Habits habits={habits} />
       </div>
       <Footer />
     </div>
